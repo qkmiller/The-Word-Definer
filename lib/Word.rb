2c01c9f554c0593868828203f3e71b0b9f6fce4f
Quinn Miller <example@example.com>
2018-11-30 10:02:49 -0800

    updated word.rb and word_spec.rb to pass previous test

--- lib/Word.rb
+++ lib/Word.rb
@@ -2,12 +2,15 @@ module WordModule
   class Word
 
     attr_accessor :word_name, :definition_list
+    attr_accessor :id
 
     @@word_list = []
+    @@word_id_list = []
 
     def initialize(word)
       @word_name = word
       @definition_list = []
+      @id = (@@word_list.length) +1
     end
 
     def add_definition(definition)
@@ -16,10 +19,20 @@ module WordModule
 
     def save
       @@word_list.push(self.word_name)
+      @@word_id_list.push(self.id)
+    end
+
+    def self.clear
+      @@word_list = []
+      @@word_id_list = []
     end
 
     def self.all
       @@word_list
+    end
+
+    def self.see_word_ids
+      @@word_id_list
     end
   end
 end
--- spec/Word_spec.rb
+++ spec/Word_spec.rb
@@ -2,39 +2,41 @@ require('Word')
 require('rspec')
 require('pry')
 
-describe (WordModule::Word) do
-
-  describe('#definition_list') do
-    it('should return an array') do
-      new_word = WordModule::Word.new("Blank")
-      expect(new_word.definition_list).to(eq([]))
-    end
+describe('#definition_list') do
+  it('should return an array') do
+    new_word = WordModule::Word.new("Blank")
+    expect(new_word.definition_list).to(eq([]))
+    WordModule::Word.clear
   end
+end
 
-  describe('#add_definition') do
-    it('should add a definition to definition_list') do
-      new_word = WordModule::Word.new("Blank")
-      new_word.add_definition("blank definition")
-      expect(new_word.definition_list).to(eq(["blank definition"]))
-    end
+describe('#add_definition') do
+  it('should add a definition to definition_list') do
+    new_word = WordModule::Word.new("Blank")
+    new_word.add_definition("blank definition")
+    expect(new_word.definition_list).to(eq(["blank definition"]))
+    WordModule::Word.clear
   end
+end
 
-  describe('#all') do
-    it('should list all instances of Word') do
-      new_word = WordModule::Word.new("Blank")
-      new_word2 = WordModule::Word.new("Test")
-      new_word.save
-      new_word2.save
-      expect(WordModule::Word.all).to(eq(["Blank", "Test"]))
-    end
+describe('#all') do
+  it('should list all instances of Word') do
+    new_word = WordModule::Word.new("Blank")
+    new_word.save
+    new_word2 = WordModule::Word.new("Test")
+    new_word2.save
+    expect(WordModule::Word.all).to(eq(["Blank", "Test"]))
+    WordModule::Word.clear
   end
-  describe('#see_word_ids') do
-    it('should list the unique IDs for all instances of word') do
-      new_word = WordModule::Word.new("Blank")
-      new_word2 = WordModule::Word.new("Test")
-      new_word.save
-      new_word2.save
-      expect(WordModule::Word.see_word_ids).to(eq([1, 2]))
-    end
+end
+
+describe('#see_word_ids') do
+  it('should list the unique IDs for all instances of word') do
+    new_word = WordModule::Word.new("Blank")
+    new_word.save
+    new_word2 = WordModule::Word.new("Test")
+    new_word2.save
+    expect(WordModule::Word.see_word_ids).to(eq([1, 2]))
+    WordModule::Word.clear
   end
 end
