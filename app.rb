d5f3836a1202f363ee6692e1c8aa82abd8d8ef7e
Quinn Miller <example@example.com>
2018-11-30 10:42:02 -0800

    add integration test that checks if words are being shown on the homepage

--- app.rb
+++ app.rb
@@ -5,6 +5,14 @@ also_reload('lib/**/*.rb')
 require('./lib/Word')
 require('pry')
 
+@@word_list = []
+
 get('/') do
   erb(:input)
+end
+
+
+
+get('/word/:id') do
+  erb(:word)
 end
--- spec/Word_integration_spec.rb
+++ spec/Word_integration_spec.rb
@@ -6,6 +6,14 @@ set(:show_exceptions, false)
 describe('the word definer', {:type => :feature}) do
   it('makes sure capybara and sinatra are working') do
     visit('/')
-    expect(page).to have_content('Welcome to the Word Definer')
+    expect(page).to have_content('Welcome to the Word Definer!')
+  end
+end
+describe('the word definer', {:type => :feature}) do
+  it('checks if words are showing on the hompepage') do
+    visit('/')
+    fill_in('word', :with => 'test')
+    click_button('Submit')
+    expect(page).to have_content("test")
   end
 end
