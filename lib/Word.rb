9f12a95d273ab6cf4f15ddfa6ef98db4ea595261
Quinn Miller <example@example.com>
2018-11-30 11:28:09 -0800

    update update Word.rb to contain a separate list for the actual word objects

--- lib/Word.rb
+++ lib/Word.rb
@@ -6,6 +6,7 @@ module WordModule
 
     @@word_list = []
     @@word_id_list = []
+    @@word_objects = []
 
     def initialize(word)
       @word_name = word
@@ -20,19 +21,25 @@ module WordModule
     def save
       @@word_list.push(self.word_name)
       @@word_id_list.push(self.id)
+      @@word_objects.push(self)
     end
 
     def self.clear
       @@word_list = []
       @@word_id_list = []
+      @@word_objects = []
     end
 
     def self.all
       @@word_list
     end
-    
+
     def self.see_word_ids
       @@word_id_list
+    end
+
+    def self.find(id)
+      @@word_objects[id - 1].word_name
     end
   end
 end
