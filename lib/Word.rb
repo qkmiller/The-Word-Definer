b4fc0eb8eb533b2716b1c35903aba741b8a51592
Quinn Miller <example@example.com>
2018-11-30 09:17:26 -0800

    add test for definition() to return an empty array

---
+++ lib/Word.rb
@@ -0,0 +1,12 @@
+module WordModule
+  class Word
+
+    attr_accessor :word_name, :definition
+
+    def initialize(word)
+      @word_name = word
+      # @definition = []
+    end
+
+  end
+end
--- spec/Word_spec.rb
+++ spec/Word_spec.rb
@@ -1,3 +1,12 @@
 require('Word')
 require('rspec')
 require('pry')
+
+describe (WordModule::Word) do
+  describe('#definition') do
+    it('should return an array') do
+      new_word = Word.new("Blank")
+      expect(new_word.definition).to(eq([]))
+    end
+  end
+end
