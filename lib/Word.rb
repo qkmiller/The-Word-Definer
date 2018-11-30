3312e2415b9beaf599fb3ac726bf954022464344
Quinn Miller <example@example.com>
2018-11-30 09:23:13 -0800

    add test for add_definition()

--- lib/Word.rb
+++ lib/Word.rb
@@ -1,11 +1,11 @@
 module WordModule
   class Word
 
-    attr_accessor :word_name, :definition
+    attr_accessor :word_name, :definition_list
 
     def initialize(word)
       @word_name = word
-      @definition = []
+      @definition_list = []
     end
 
   end
--- spec/Word_spec.rb
+++ spec/Word_spec.rb
@@ -3,10 +3,20 @@ require('rspec')
 require('pry')
 
 describe (WordModule::Word) do
-  describe('#definition') do
+
+  describe('#definition_list') do
     it('should return an array') do
       new_word = WordModule::Word.new("Blank")
-      expect(new_word.definition).to(eq([]))
+      expect(new_word.definition_list).to(eq([]))
     end
   end
+
+  describe('#add_definition') do
+    it('should add a definition to definition_list') do
+      new_word = WordModule::Word.new("Blank")
+      new_word.add_definition("blank definition")
+      expect(new_word.definition).to(eq(["blank definition"]))
+    end
+  end
+
 end
